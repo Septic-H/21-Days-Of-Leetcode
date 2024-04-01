2dc74e696aa6ccfa13dbeac3d72f1d99219c7f01
Huzaifa <127947250+Septic-H@users.noreply.github.com>
2024-04-01 08:41:40 -0700

    Time: 24 ms (76.23%), Space: 23.8 MB (69.49%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        unordered_map<string, vector<string>> map;
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            map[temp].push_back(strs[i]);
+        }
+
+        for (auto it = map.begin(); it != map.end(); it++) {
+            result.push_back(it->second);
+        }
+        
+        return result;
+    }
+};
